6223c2599e99afbb747b4383badfdaec6670fe74
Kate Studwell <katestudwell@gmail.com>
2025-03-04 16:49:03 -0500

    Display the game in electron

--- src/App.tsx
+++ src/App.tsx
@@ -32,11 +32,23 @@ function App() {
     }
   }
 
+    // Function to shuffle the array
+  function shuffle(array: any[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1))
+      ;[array[i], array[j]] = [array[j], array[i]]
+    }
+    return array
+  }
+
+  // Shuffle the cards array and select the first 12 cards
+  const randomCards = shuffle(cards).slice(0, 12)
+
   return (
     <>
       <h1>Lot</h1>
       <div className="card-container">
-        {cards}
+        {randomCards}
       </div>
     </>
   )
